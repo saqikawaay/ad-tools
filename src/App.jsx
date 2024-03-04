4fc4cd2cb436f95b63fe7591ad072d214f46c1c8
Trevor Anderson <saqikawaay@gmail.com>
2024-03-04 22:55:39 +1000

    auth not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Login from './components/Login';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -8,18 +8,26 @@ import TopBar from './components/TopBar';
 import { Typography } from '@mui/material';
 import theme from './theme';
 import Layout from './Layout';
-
+import { checkSession, supabaseAuthListener } from './components/authClient';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+ const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+ useEffect(() => {
+  //check if there's a current session: 
+  const session = checkSession();
+  setIsLoggedIn(!!session);
+  //subscribe to session changes
+  const { data: listener } = supabaseAuthListener((event, session) => {
+  setIsLoggedIn(!!session);
+  });
+
+  return () => {
+    listener.unsubscribe();
+  };
+}, []);
 
 
-
-
-  // const handleLogout = () => {
-  //   // logout logic would go here
-  //   setIsLoggedIn(false);
-  // };
 
 
 
@@ -30,7 +38,7 @@ function App() {
        <Container maxWidth="sm">
        <Box sx={{ bgcolor: 'white', height: '100vh' }}>      
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Welcome to the app!
+        🚨 Welcome to the AdSiren! 🚨
         </Typography>
         <Layout>
           
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,10 @@
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-    'https://xxaphohtmaresrxqctzi.supabase.co', 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4YXBob2h0bWFyZXNyeHFjdHppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwOTM2NzksImV4cCI6MjAyNDY2OTY3OX0.yeMPTa56LptAxgkf7COWqAR6GN2mYGMFjrONNSiVYpc'
-    );
+import React from 'react';
 
      
-const Login = () => (
+const Login = ({supa}) => (
     <>
     <h2>Welcome to AdVitals</h2>
     <p>Log in to to your account or sign in with Google</p>
