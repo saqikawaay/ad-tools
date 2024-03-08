d5a5a9a408a674572f35a28af9e0324388b022ab
saqikawaay <trevorjonanderson@gmail.com>
2024-03-08 17:49:50 +1000

    minor layout and title

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,7 +6,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -14,7 +13,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-const drawerWidth = 220;
+const drawerWidth = 210;
 
 export default function ClippedDrawer() {
   return (
